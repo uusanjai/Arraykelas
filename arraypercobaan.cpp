b8b250a1801fbeeedeb58eb7b734a082c2a50a08
uusanjai <c030324034@mahasiswa.poliban.ac.id>
2025-04-04 22:33:49 +0800

    pembuatan prosedur untuk proses input data & pengujian prosedur tersebut

--- arraypercobaan.cpp
+++ arraypercobaan.cpp
@@ -1,3 +1,32 @@
 #include <iostream>
 using namespace std;
 string kelas [30]; 
+int pos = 0;
+void inputData(string nama);
+void outputData();
+
+int main(){
+inputData("Muhammad Firdaus");
+outputData();
+inputData("Hai name saye Walid");
+outputData();
+inputData("walid nak you nak ta?");
+outputData();
+return 0;
+
+}
+void inputData(string nama){
+  kelas[pos]=nama;
+    pos++;
+}
+
+void outputData(){
+    for (int i=0; i<pos; i++){
+        cout << kelas[i];
+        if (i < pos - 1){
+            cout << ", ";
+        } else {
+            cout << "," ;
+        }
+   }
+}
